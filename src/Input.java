a5211b4d9acc416e5582c5fd0e929b21616a79b5
Kayla <144965767+KaylaL27@users.noreply.github.com>
2025-01-29 16:38:52 +0000

    finished lab

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Input{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+        
+        System.out.println("Enter your weight: ");
+        double weight = Double.parseDouble(scanner.nextLine());
+        
+        System.out.println("Are you a smoker? (yes/no): ");
+        String response = scanner.nextLine();
+        boolean smoker = response.equalsIgnoreCase("yes");
+
+        System.out.println("Your name: " + name + " Your age: " + age + " Your weight: " + weight + " Smoker: " + smoker);
+
+    }
+}
